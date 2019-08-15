487d76d453468ab508d0bee22bd675ca71f353a1
Jaime Vasquez <rodrigoxv@gmail.com>
2019-08-15 14:28:52 -0400

    delete dist

--- angular-atomic-workspace/dist/angular-atomic/angular-atomic.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { ButtonComponent as ɵa } from './lib/atoms/button/button.component';
-export { BannerLargeComponent as ɵb } from './lib/molecules/banner-large/banner-large.component';
--- angular-atomic-workspace/dist/angular-atomic/lib/molecules/banner-large/banner-large.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class BannerLargeComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
