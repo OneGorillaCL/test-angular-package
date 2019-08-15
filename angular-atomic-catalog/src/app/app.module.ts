537c30c6cd590c97ec1468fa2f7f99444ddd877c
Jaime Vasquez <rodrigoxv@gmail.com>
2019-08-15 15:23:31 -0400

    angular-atomic package

--- angular-atomic-catalog/src/app/app.module.ts
+++ angular-atomic-catalog/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularAtomicModule } from 'angular-atomic';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularAtomicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
